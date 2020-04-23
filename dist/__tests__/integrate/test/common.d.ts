824b4c8f67cf45b3cc20265cd5df14029b123062
mikamo3 <kamo3proj@gmail.com>
2020-04-23 17:42:03 +0000

    :building_construction: (tests) exclude test dir

--- dist/__tests__/integrate/test/common.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class AssertionError extends Error {
-    constructor(actual: unknown, expected: unknown);
-}
-export declare const assert: (message: string) => {
-    toEqual: (actual: unknown, expected: unknown) => void;
-    toMatchObject: (actual: object, expected: object) => void;
-};
-export declare const test: (message: string, beforeRun: Function, test: Function, afterRun?: Function) => void;
