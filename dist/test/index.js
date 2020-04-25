030bdebf96228fb8a5d4f21933c93ad7d1204d71
mikamo3 <kamo3proj@gmail.com>
2020-04-25 09:41:12 +0000

    :sparkles: (add test util)

--- dist/test/index.js
+++ dist/test/index.js
@@ -5,4 +5,5 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./assert"));
 __export(require("./testRunner"));
+__export(require("./spreadsheet"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from "./spreadsheet";
-export * from "./testhelper";
 export * from "./test";
